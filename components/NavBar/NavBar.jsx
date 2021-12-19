9802e95f9bbdf336c6efad1a7beb45360d1aa565
Jason Rice <jrice@entrata.com>
2021-12-18 22:15:57 -0700

    Debugging

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -236,31 +236,35 @@ const MobileNavItem = ({label, children, href}) => {
 
   return (
     <Stack spacing={4} onClick={children && onToggle}>
-      <Flex
-        py={2}
-        as={Link}
+      <NextLink
         href={href ?? '#'}
-        justify={'space-between'}
-        align={'center'}
-        _hover={{
-          textDecoration: 'none'
-        }}>
-        <Text
-          fontWeight={600}
-          color={useColorModeValue('gray.600', 'gray.200')}>
-          {label}
-        </Text>
-        {children && (
-          <Icon
-            as={ChevronDownIcon}
-            transition={'all .25s ease-in-out'}
-            transform={isOpen ? 'rotate(180deg)' : ''}
-            w={6}
-            h={6}
-          />
-        )}
-      </Flex>
-
+        shallow={true}
+        passHref
+      >
+        <Flex
+          py={2}
+          as={Link}
+          justify={'space-between'}
+          align={'center'}
+          _hover={{
+            textDecoration: 'none'
+          }}>
+          <Text
+            fontWeight={600}
+            color={useColorModeValue('gray.600', 'gray.200')}>
+            {label}
+          </Text>
+          {children && (
+            <Icon
+              as={ChevronDownIcon}
+              transition={'all .25s ease-in-out'}
+              transform={isOpen ? 'rotate(180deg)' : ''}
+              w={6}
+              h={6}
+            />
+          )}
+        </Flex>
+      </NextLink>
       <Collapse in={isOpen} animateOpacity style={{marginTop: '0!important'}}>
         <Stack
           mt={2}
